696f7e0089131574c208d0451321154bd884104b
Henrik Width <henrik.width@gmail.com>
2014-02-16 21:24:26 +0100

    First crack at date on X-axis

--- logreader.py
+++ logreader.py
@@ -1,13 +1,18 @@
 from DataFromCSV import DataFromCSV
 import matplotlib.pyplot as pl
 import sys
+import matplotlib.dates as mdates
 
 if len(sys.argv) < 2:
     print "Usage: python "+sys.argv[0]+" filename.csv"
 else:
     data = DataFromCSV(sys.argv[1]).getdata()
-    pl.plot(data["BeerTemp"])
-    pl.plot(data["BeerSetting"])
-    pl.plot(data["FridgeTemp"])
-    pl.plot(data["FridgeSetting"])
+    
+    date = [d.to_datetime() for d in data["Date"]]
+    
+    pl.plot(date,data["BeerTemp"])
+    pl.plot(date,data["BeerSetting"])
+    pl.plot(date,data["FridgeTemp"])
+    pl.plot(date,data["FridgeSetting"])
+
     pl.show()
