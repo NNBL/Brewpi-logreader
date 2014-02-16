eedcaa25481a1146a8cffff6863d7606ce57d0da
Henrik Width <henrik.width@gmail.com>
2014-02-16 13:47:29 +0100

    using pandas for easy date parsing from csv

--- logreader.py
+++ logreader.py
@@ -6,7 +6,6 @@ if len(sys.argv) < 2:
     print "Usage: python "+sys.argv[0]+" filename.csv"
 else:
     data = DataFromCSV(sys.argv[1]).getdata()
-
     pl.plot(data["BeerTemp"])
     pl.plot(data["BeerSetting"])
     pl.plot(data["FridgeTemp"])
