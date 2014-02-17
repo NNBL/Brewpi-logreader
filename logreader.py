3d0e9c6374ad2544b3701aa9d09c5e1b57bb71a1
Henrik Width <henrik.width@gmail.com>
2014-02-17 10:24:04 +0100

    x-labels are not cut off, grid makes it easier to read

--- logreader.py
+++ logreader.py
@@ -2,6 +2,9 @@ from DataFromCSV import DataFromCSV
 import matplotlib.pyplot as pl
 import sys
 import matplotlib.dates as mdates
+from matplotlib import rcParams
+
+rcParams.update({'figure.autolayout': True})
 
 if len(sys.argv) < 2:
     print "Usage: python "+sys.argv[0]+" filename.csv"
@@ -9,10 +12,11 @@ else:
     data = DataFromCSV(sys.argv[1]).getdata()
     
     date = [d.to_datetime() for d in data["Date"]]
-    
+    pl.title("Fermentation profile for: "+sys.argv[1])
+    pl.xticks(rotation=50)
     pl.plot(date,data["BeerTemp"])
     pl.plot(date,data["BeerSetting"])
     pl.plot(date,data["FridgeTemp"])
     pl.plot(date,data["FridgeSetting"])
-
+    pl.grid()
     pl.show()
